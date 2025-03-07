27014f80bcc983120463ab69171b7f6d700841f5
Daniel <isral.tudela712.96@gmail.com>
2025-03-06 21:11:40 -0400

    i did the letter grade program

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.IO.Compression;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("what is your grade porcentage?");
+        string gradePorcentage = Console.ReadLine();
+        int numberPorcentage= int.Parse(gradePorcentage);
+        if (numberPorcentage>=70 && numberPorcentage<= 80)
+        {
+            Console.WriteLine("congratulation your letter grade is C");           
+        }
+        else if (numberPorcentage>=80 && numberPorcentage<= 90)
+        {
+            Console.WriteLine("congratulation your letter grade is B");
+        }
+        else if (numberPorcentage>=90 && numberPorcentage<= 100)
+        {
+            Console.WriteLine("congratulation your letter grade is A");
+        }
+        else if (numberPorcentage >= 60 && numberPorcentage <= 70)
+        {
+            Console.WriteLine("keep learning you can doit for the nex time your letter grade is D");
+        }
+        else 
+        {
+            Console.WriteLine("try the next time we love you your letter grade is F");
+        }
     }
 }
