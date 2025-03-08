b5252df9ff462d25861311e6fd7a15220e2113cb
Daniel <isral.tudela712.96@gmail.com>
2025-03-07 20:04:40 -0400

    random number added

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+
+        //Console.WriteLine("what is your magic number?");
+        //string numberUserInput= Console.ReadLine();
+        //int numberConverted= int.Parse(numberUserInput);
+        Console.WriteLine("what is your guess?");
+        string guessUserInput= Console.ReadLine();
+        int guessConverted= int.Parse(guessUserInput);
+        int number = randomGenerator.Next(1, 20);
+        
+        while (guessConverted!=number)
+            {
+                Console.WriteLine("what is your guess? ");
+                 guessConverted = int.Parse(Console.ReadLine());
+            
+                if (guessConverted<number)
+                {
+                    Console.WriteLine(" Higher ");
+                }
+                else if (guessConverted>number)
+                {
+                    Console.WriteLine(" Lower ");
+                }
+            }
+        Console.WriteLine("you guessed it");
+            
+         
     }
 }
